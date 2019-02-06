81bc1e21ee7bafd9f94ed22c7fd1e5e91c7a20d9
Swaggin <jarzab76pl@o2.pl>
2019-02-06 22:26:37 +0100

    22:26

--- admin/login.php
+++ admin/login.php
@@ -1,32 +1,31 @@
 <?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Registration system PHP and MySQL</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <script src="../scripts/login_check.js"></script>
-</head>
-<body>
-<div class="header">
-    <h2>Login</h2>
-</div>
-
-<form method="post" action="login.php">
-    <?php include('errors.php'); ?>
-    <div class="input-group">
-        <label>Username</label>
-        <input type="text" name="username" onchange="username_check()" >
-    </div>
-    <div class="input-group">
-        <label>Password</label>
-        <input type="password" name="password">
-    </div>`
-    <div class="input-group">
-        <button type="submit" class="btn" name="login_user">Login</button>
-    </div>
-    <p>
-        Not yet a member? <a href="register.php">Sign up</a>
-    </p>
-</form>
-</body>
+    <head>
+        <title>Registration system PHP and MySQL</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="../scripts/login_check.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Lobster|Montserrat|Quicksand" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="reg-title">Login</h1>
+            <form method="post" action="login.php">
+                <?php include('errors.php'); ?>
+                <div class="input-group">
+                    <input type="text" name="username" onchange="username_check()" placeholder="Enter your username">
+                </div>
+                <div class="input-group">
+                    <input type="password" name="password" placeholder="Enter password">
+                </div>
+                <div class="input-group">
+                    <button type="submit" class="btn" name="login_user">Login</button>
+                </div>
+                <p class="member-info">
+                    Not yet a member? <a href="register.php">Sign up</a>
+                </p>
+            </form>
+        </div>
+    </body>
 </html>
