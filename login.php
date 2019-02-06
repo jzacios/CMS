2488814e9e18884e332023970df54d79d9626e21
jzacios <jakub.zacios@gmail.com>
2019-02-06 12:41:37 +0100

    gujh

---
+++ login.php
@@ -0,0 +1,20 @@
+<html lang=pl>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div class="containter">
+    <div class="login_box">
+        <div class="login_box_content">
+            Login <input type="text"> <br>
+            Password <input type="text">
+        </div>
+    </div>
+</div>
+</body>
+</html>
