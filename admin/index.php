08199a2fd066ad3b11ffb30f6728fc6a8aed6b3f
Swaggin <jarzab76pl@o2.pl>
2019-02-07 22:27:34 +0100

    22:27

--- admin/index.php
+++ admin/index.php
@@ -13,41 +13,42 @@ if (isset($_GET['logout'])) {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+    <head>
+        <title>Home</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Lobster|Montserrat|Quicksand" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    </head>
+    <body>
 
+    <div class="content">
+        <!-- notification message -->
+        <?php if (isset($_SESSION['success'])) : ?>
+            <div class="error success" >
+                <h3>
+                    <?php
+                    echo $_SESSION['success'];
+                    unset($_SESSION['success']);
+                    ?>
+                </h3>
+            </div>
+        <?php endif ?>
 
-<div class="content">
-    <!-- notification message -->
-    <?php if (isset($_SESSION['success'])) : ?>
-        <div class="error success" >
-            <h3>
-                <?php
-                echo $_SESSION['success'];
-                unset($_SESSION['success']);
-                ?>
-            </h3>
-        </div>
-    <?php endif ?>
+        <!-- logged in user information -->
+        <?php  if (isset($_SESSION['username'])) : ?>
+            <!--dodawanie do galerii-->
+            <form method="post" action="../scripts/gallery_add.php">
+                <h1>Dodaj post do galerii</h1><br>
+                <p>Tytuł:</p><input type="text" name="tytul">
+                <p>Ścieżka:</p><input type="text" name="sciezka">
+                <p>Opis:</p><textarea rows="4" cols="50" name="opis"></textarea><br />
+                <button type="submit">Submit</button>
+            </form>
+            <!--dodawanie do galerii-->
+            <a href="galeria.php">Galeria</a>
+            <a href="posty.php">Lista postow</a><br>
+        <?php endif ?>
+    </div>
 
-    <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
-        <a href="posty.php">Lista postow</a><br>
-<!--    dodawanie do galerii-->
-        <form method="post" action="../scripts/gallery_add.php">
-            Dodaj post do galerii <br>
-            tytul <input type="text" name="tytul">
-            sciezka <input type="text" name="sciezka">
-            opis <textarea rows="4" cols="50" name="opis"></textarea>
-            <input type="submit">
-        </form>
-<!--    dodawanie do galerii-->
-        <a href="galeria.php">Galeria</a>
-    <?php endif ?>
-</div>
-
-</body>
+    </body>
 </html>
